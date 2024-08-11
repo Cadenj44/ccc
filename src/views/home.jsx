11f1e0e9ef77bc22f63ae66f5bb2f9f902cc9929
Cadenj44 <cadencameron44@gmail.com>
2024-08-11 09:37:46 -0500

    Began adding backgrounds and stuff

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,14 +1,30 @@
+import * as React from 'react';
 import './home.css';
+import { Box } from '@mui/material';
+import bgImage from '../assets/bible.jpg';
  
 function Home() {
     return (
-        <div>
+        <Box
+        sx={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          height: '100vh',  // Full viewport height
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: '#ffffff',    // White text color
+          textAlign: 'center',
+        }}
+        >
           <h1>
               LOVE GOD. <br/>
               LOVE ONE ANOTHER. <br/>
               MAKE DISCIPLES.
           </h1>
-         </div>
+         </Box>
+
     );
   }
   
